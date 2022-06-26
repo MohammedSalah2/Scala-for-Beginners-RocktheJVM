a23b6ff707482b87776a53abc16cc3c668f565c3
MohammedSalah2 <mohammed.salah@clearscore.com>
2022-06-26 15:01:25 +0100

    Recursion questions and answers

--- src/lectures/part1basics/Recursion.scala
+++ src/lectures/part1basics/Recursion.scala
@@ -1,8 +1,11 @@
 package lectures.part1basics
 
+import scala.annotation.tailrec
+
 object Recursion extends App{
 
-  def factorial(n: Int): Int =
+
+  def factorial(n: Int): Int = {
     if (n <= 1) 1
     else {
       println("Computing factorial of " + n + " - I first need factorial of " + (n-1))
@@ -11,17 +14,49 @@ object Recursion extends App{
 
       result
     }
+  }
 
   println(factorial(10))
 // too large of a factorial number will result in a StackOverflowError for the function above
 
+
   def anotherFactorial (n: Int): Int = {
     def factHelper(x: Int, accumulator: Int): Int =
       if (x <= 1) accumulator
-      else factHelper(x - 1, x * accumulator)
+      else factHelper(x - 1, x * accumulator) // TAIL RECURSION = use recursive call as the LAST expression
+    // we wrote factHelper here as the last expression of its code path, this allows Scala to preserve the same stack
+    // frame, and not use additional stack frames for recursive cause
 
     factHelper(n, 1)
   }
+
+  // WHEN YOU NEED LOOPS, USE_TAIL_RECURSION
+
+  /*
+    1. Concatenate a string n times
+    2. IsPrime function tail recursive
+    3. Fibonacci function, tail recursive
+   */
+
+  def greeting(x: String, n: Int, accumulator: String): String =
+    if (n <= 0) accumulator
+    else greeting(x, n - 1, x + accumulator)
+
+  println(greeting("hello", 3, ""))
+  // In this instance we are looping, therefore we add an accumulator
+
+  def isPrime(n: Int): Boolean = {
+    def isPrimeTailrec(t: Int, isStillPrime: Boolean): Boolean =
+      if (!isStillPrime) false
+      else if (t <= 1) true
+      else isPrimeTailrec(t - 1, n & t != 0 && isStillPrime)
+
+      isPrimeTailrec(n / 2, true)
+  }
+
+  println(isPrime(2003))
+  println(isPrime(269))
+
 
 
 
