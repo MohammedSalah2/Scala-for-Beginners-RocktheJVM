5409b11a624f8bc1ae2ee890c8ac4a0243d3dd2c
MohammedSalah2 <mohammed.salah@clearscore.com>
2022-07-18 11:42:38 +0100

    Method Notations:Exercises

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -3,10 +3,12 @@ import scala.language.postfixOps
 
 object MethodNotations extends App{
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(nickname: String): Person = new Person(s"$name ($nickname", favoriteMovie)
     def unary_! : String = s"$name, what the heck?!"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
   }
@@ -44,7 +46,25 @@ object MethodNotations extends App{
   println(mary()) //equivalent
   // whenever the compiler sees an object being called like a function, it looks for a definition of apply
 
+  /*
+    1. Overload the + operator
+       mary + "the rockstar" => new person "Mary (the rockstar)"
 
+       2. Add an age to the Person class
+          Add a unary + operator => new person with the age + 1
+          +mary => mary with the age incrementer
+
+    3. Add a "learns" method in the Person class => "Mary learns "Scala
+       Add a learnsScala method, calls learns method with "Scala".
+       Use it in the postfix notation.
+
+    4. Overload the apply method
+       mary.apply(2) => "Mary watched Inception 2 times"
+   */
+
+  println((mary + "the Rockstar").apply())
+
+  println((+mary).age)
 
 
 }
