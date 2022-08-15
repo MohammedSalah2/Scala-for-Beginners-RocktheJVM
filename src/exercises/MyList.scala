03653d759ab4c912917b91e619680324834c80b7
MohammedSalah2 <mohammed.salah@clearscore.com>
2022-08-15 11:47:43 +0100

    Exceptions

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -1,4 +1,6 @@
+/*
 package exercises
+
 
 abstract class MyList[+A] {
 
@@ -11,30 +13,43 @@ abstract class MyList[+A] {
    */
 
   def head: A
-  def tail: MyList[A]
-  def isEmpty: Boolean
-  def add[B >: A](element: B): MyList[B]
-  protected def printElements: String
-  // polymorphic call
-  override def toString: String = "[" + printElements +  "]"
 
-  def map[B](transformer: MyTransformer[A, B]): MyList[B]
-  def flatMap[B] (transformer: MyTransformer[A, MyList[B]]): MyList[B]
+  def tail: MyList[A]
+
+  def isEmpty: Boolean
+
+  def add[B >: A](element: B): MyList[B]
+
+  protected def printElements: String
+
+  // polymorphic call
+  override def toString: String = "[" + printElements + "]"
+
+//  def map[B](transformer: MyTransformer[A, B]): MyList[B]
+
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+
   def filter(predicate: MyPredicate[A]): MyList[A]
 
   //concatenation
-  def++[B >: A](list: MyList[B]): MyList[B]
+  def ++[B >: A](list: MyList[B]): MyList[B]
 
 
   case object Empty extends MyList[Nothing] {
     def head: Nothing = throw new NoSuchElementException
+
     def tail: MyList[Nothing] = throw new NoSuchElementException
+
     def isEmpty: Boolean = true
-    def add[B >: Nothing] (element: B): MyList[B] = new Cons(element, Empty)
+
+    def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
+
     def printElements: String = ""
 
     def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
+
     def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+
     def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
 
     def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
@@ -42,27 +57,32 @@ abstract class MyList[+A] {
 
   case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     def head: A = h
+
     def tail: MyList[A] = t
+
     def isEmpty: Boolean = false
+
     def add[B >: Nothing](element: B): MyList[B] = new Cons(element, this)
+
     def printElements: String =
-      if(t.isEmpty) "" + h
+      if (t.isEmpty) "" + h
       else h + " " + t.printElements
-  //}
+    //}
 
-  //trait MyPredicate[-T] {
+    //trait MyPredicate[-T] {
     //def test[elem: T]: Boolean
-  //}
+    //}
 
-  //trait MyTransformer[-A, B] {
+    //trait MyTransformer[-A, B] {
     //def transform(elem: A): B
-  //}
+    //}
 
-  //object ListTest extends App {
+    //object ListTest extends App {
     //val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
     //val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
-  //}
+    //}
 
 
-
+  }
 }
+*/
---
+++ src/lectures/part2oop/Exceptions.scala
@@ -0,0 +1,84 @@
+package lectures.part2oop
+
+import java.nio.BufferUnderflowException
+
+
+object Exceptions extends App {
+
+  val x: String = null
+  // println(x.length)
+  // this ^^ will crash with a Null Pointer Exception
+
+  // 1. throwing and catching exceptions
+
+  // exceptions are instances of classes
+
+  //val aWeirdValue: String = throw new NullPointerException
+
+  // throwable classes extend the Throwable class.
+  // Exception and Error are the major Throwable subtypes
+  // both exceptions and errors will crash but exceptions denote something that went wrong with the program eg. NPE
+  // errors denote something that went wrong with the system
+
+  // 2. how to catch exceptions
+  def getInt(withExceptions: Boolean): Int =
+    if(withExceptions) throw new RuntimeException("No int for you!")
+    else 42
+
+  val potentialFail = try {
+    // code that might throw
+    getInt(true)  // inside try block you put your code that might throw an exception
+  } catch {
+    case e: RuntimeException => 43 // in catch block you try match against all exceptions you might have
+  } finally {
+    // code that will get executed NO MATTER
+    // optional
+    // does not influence the return type of this expression
+    // use finally only for side effects
+    println("finally")
+  }
+  println(potentialFail)
+
+  // 3. how to define your own exceptions
+  class MyException extends Exception
+  val exception = new MyException
+
+  // throw exception
+
+  /*
+    1. Crash your programme with an OutOfMemoryError
+    2. Crash with SOError
+    3. PocketCalculator
+      - add(x, y)
+      - subtract(x, y)
+      - multiply(x, y)
+      - divide(x, y)
+
+      Throw
+        - OverflowException if add(x, y) exceeds Int.MAX_VALUE
+        - UnderflowException if subtract(x, y) exceeds Int.MIN_VALUE
+        - MathCalculationException for division by 0
+   */
+
+  // 1. OOM
+  // val array = Array.ofDim(Int.MaxValue)
+
+  // 2. StackOverflow Error
+  // def infinite: Int = 1 + infinite
+  // val noLimit = infinite
+
+  class OverflowException extends RuntimeException
+  class UnderflowException extends RuntimeException
+
+  object PocketCalculator {
+    def add(x: Int, y: Int) = {
+      val result = x + y
+      if (x > 0 && y > 0 && result < 0) throw new OverflowException
+      if ( x < 0 && y < 0 && result > 0) throw new UnderflowException
+      else result
+
+    }
+  }
+  println(PocketCalculator.add(Int.MaxValue, 2))
+
+}
