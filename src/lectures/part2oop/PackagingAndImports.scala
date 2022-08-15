0bd6cfcaf32698cc2af9ca66168459e4632f6640
MohammedSalah2 <mohammed.salah@clearscore.com>
2022-08-15 15:26:12 +0100

    Packaging and Imports

---
+++ src/lectures/part2oop/PackagingAndImports.scala
@@ -0,0 +1,36 @@
+package lectures.part2oop
+
+import playground.{Cinderella => Princess, PrinceCharming}
+
+import java.util.Date
+import java.sql.Date
+
+object PackagingAndImports extends App {
+
+  // package members are accessible by their simple name
+  val writer = new Writer("Daniel", "RockTheJVM", 2018)
+
+  // import the package
+  val princess = new Princess // playground.Cinderella = fully qualified name
+
+  // packages are in hierarchy
+  // matching folder structure
+
+  // package object
+  sayHello
+  println(SPEED_OF_LIGHT)
+
+  // imports
+  val prince = new PrinceCharming
+
+  // 1. use Fully Qualified names
+
+  val date = new Date()
+  val sqlDate = new java.sql.Date(2018, 5, 4)
+  // 2. use aliasing
+
+  // default imports
+  // java.lang - String, Object, Exception
+  // scala - Int, Nothing, Function
+  // scala.Predef - println, ???
+}
