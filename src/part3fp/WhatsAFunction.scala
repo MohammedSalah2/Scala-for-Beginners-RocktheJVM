3a6b6da2902966be78d831f77613608fbdc50937
MohammedSalah2 <mohammed.salah@clearscore.com>
2022-08-16 10:55:39 +0100

    Functions Exercises

--- src/part3fp/WhatsAFunction.scala
+++ src/part3fp/WhatsAFunction.scala
@@ -30,6 +30,41 @@ object WhatsAFunction extends App {
 
   // ALL SCALA FUNCTIONS ARE OBJECTS
 
+  /*
+    1. a function which takes 2 strings and concatenates them
+    2. transform the MyPredicate and MyTransformer into function types
+    3. define a function which takes and int and returns another function which takes an int and returns an int
+      - what's the type of this function
+      - how to do it
+   */
+
+
+      def concatenator = new ((String, String) => String) {
+        override def apply(a: String, b: String): String = a + b
+      }
+      println(concatenator("Mohammed ", "Salah"))
+
+      // Function1[Int, Function1[Int, Int]]
+      val superAdder: Function1[Int, Function1[Int, Int]] = new Function[Int, Function1[Int, Int]] {
+        override def apply(x: Int): Function1[Int, Int] = new Function[Int, Int] {
+          override def apply(y: Int): Int = x + y
+        }
+      }
+
+  val adder3 = superAdder(1000)
+  println(adder3(4))
+  println(superAdder(3)(4)) // curried function - can be called with multiple parameter lists
+
+
+
+
+
+
+
+
+
+
+
 
   trait MyFunction[A, B] {
     def apply(element: A): B
